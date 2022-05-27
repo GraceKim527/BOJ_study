f1cc22f5443a098bebebf3c5ea219d14e7623ddb
김은혜 <lemonherb0323@naver.com>
2022-05-27 15:26:29 +0900

    master

---
+++ 01000s/01000s/1003.py
@@ -0,0 +1,14 @@
+t = int(input()) #테스트 케이스
+zero = [1, 0, 1]
+one = [0, 1, 1]
+
+def fibonacci(n):
+    if len(zero) <= n:
+        for i in range(len(zero), n+1):
+            zero.append(zero[i-1]+zero[i-2])
+            one.append(one[i-1]+one[i-2])
+    print(zero[n], one[n])
+
+for _ in range(t):
+    n = int(input())
+    fibonacci(n)
