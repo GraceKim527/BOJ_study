1b6baebba679cc5be2b61514135a2c00c5b8cdcf
GraceKim527 <lemonherb0323@naver.com>
2022-10-04 13:05:05 +0900

    ADD 1343

---
+++ BOJ/01000s/01300s/1343.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+poli = input()
+
+poli = poli.replace('XXXX', 'AAAA')
+poli = poli.replace('XX', 'BB')
+
+if 'X' in poli:
+    print(-1)
+else:
+    print(poli)
