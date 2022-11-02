6690ea4c3dfd3a8fd3e345cb0621995c4c267118
GraceKim527 <lemonherb0323@naver.com>
2022-11-02 22:54:58 +0900

    ADD 2563 5597

---
+++ BOJ/05000s/05500s/5597.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+
+stu = [i for i in range(1,31)]
+for i in range(28):
+    applied = int(input())
+    stu.remove(applied)
+
+print(min(stu))
+print(max(stu))
