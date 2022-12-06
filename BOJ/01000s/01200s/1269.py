bdf5c8d4af8c4e1551a6ce4447cffc84d31634d3
GraceKim527 <lemonherb0323@naver.com>
2022-12-06 15:22:35 +0900

    ADD 1269

---
+++ BOJ/01000s/01200s/1269.py
@@ -0,0 +1,8 @@
+import sys
+input = sys.stdin.readline 
+
+A, B = map(int, input().split())
+a_list = set(map(int, input().split()))
+b_list = set(map(int, input().split()))
+
+print(len(a_list - b_list) + len(b_list - a_list))
