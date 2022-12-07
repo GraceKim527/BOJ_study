dd5b2ebb8e13fa34ceb931445217b8b76252c2e7
GraceKim527 <lemonherb0323@naver.com>
2022-12-07 14:29:35 +0900

    ADD 2075

---
+++ BOJ/02000s/02000s/2075.py
@@ -0,0 +1,18 @@
+import sys, heapq
+input = sys.stdin.readline
+
+N = int(input())
+heap = []
+
+for _ in range(N):
+    arr = list(map(int, input().split()))
+    if not heap:
+        for a in arr:
+            heapq.heappush(heap, a)
+    else:
+        for a in arr:
+            if heap[0] < a:
+                heapq.heappush(heap, a)
+                heapq.heappop(heap)
+
+print(heap[0])
