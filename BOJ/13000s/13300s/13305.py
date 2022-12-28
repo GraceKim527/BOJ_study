75427a6a34e189f355e6ac3f1f3bd95ef9be05c8
GraceKim527 <lemonherb0323@naver.com>
2022-12-28 22:37:22 +0900

    feat: add 13305

--- BOJ/13000s/13300s/13305.py
+++ BOJ/13000s/13300s/13305.py
@@ -1,4 +1,12 @@
-n = int(input()) # 도시의 갯수
-road = map(int, input().split()) # 거리
-liter = map(int, input().split()) # 리터
+n = int(input())
+road = list(map(int, input().split()))
+liter = list(map(int, input().split()))
 
+minVal = liter[0]
+res = 0
+for i in range(n-1):
+    if minVal > liter[i]:
+        minVal = liter[i]
+    res += (minVal * road[i])
+    
+print(res)
