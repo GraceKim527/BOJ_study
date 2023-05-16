7f2118058cd422a5ae50fce2635d88ca10f8963e
gracekim527 <lemonherb0323@naver.com>
2023-05-16 13:34:13 +0900

    boj: add 2167

---
+++ BOJ/02000s/02100s/2167.py
@@ -0,0 +1,19 @@
+import sys 
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+array = []
+for _ in range(n):
+    array.append(list(map(int, input().split())))
+
+dp = [[0] * (m + 1) for _ in range(n + 1)]
+
+for i in range(1, n + 1):
+    for j in range(1, m + 1):
+        dp[i][j] = dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1] + array[i - 1][j - 1]
+        
+k = int(input())
+for _ in range(k):
+    i, j, x, y = map(int, input().split())
+    result = dp[x][y] - dp[i - 1][y] - dp[x][j - 1] + dp[i - 1][j - 1]
+    print(result)
