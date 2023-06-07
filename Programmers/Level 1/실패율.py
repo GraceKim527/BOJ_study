5aeddca88ef9f236554aa005ecb2fc1415fab2f2
gracekim527 <lemonherb0323@naver.com>
2023-06-07 18:11:50 +0900

    :white_check_mark: [PRG] add 실패율

---
+++ Programmers/Level 1/실패율.py
@@ -0,0 +1,12 @@
+## 내 풀이
+def solution(N, stages):
+    result = {}
+    denominator = len(stages)
+    for stage in range(1, N+1):
+        if denominator != 0:
+            count = stages.count(stage)
+            result[stage] = count / denominator
+            denominator -= count
+        else:
+            result[stage] = 0
+    return sorted(result, key=lambda x : result[x], reverse=True)
