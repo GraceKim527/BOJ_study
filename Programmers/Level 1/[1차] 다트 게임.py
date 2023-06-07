2b42cd96879e046012175a52693757c785dd3c65
gracekim527 <lemonherb0323@naver.com>
2023-06-08 01:19:10 +0900

    :white_check_mark: [PRG] add [1차] 다트 게임

---
+++ Programmers/Level 1/[1차] 다트 게임.py
@@ -0,0 +1,47 @@
+## 내 풀이
+def solution(dartResult):
+    n = ''
+    score = []
+    for i in dartResult:
+        if i.isnumeric(): # 숫자일 경우
+            n += i
+        elif i == 'S':
+            n = int(n) ** 1
+            score.append(n)
+            n = ''
+        elif i == 'D':
+            n = int(n) ** 2
+            score.append(n)
+            n = ''
+        elif i == 'T':
+            n = int(n) ** 3
+            score.append(n)
+            n = ''
+        elif i == '*':
+            if len(score) > 1:
+                score[-2] = score[-2] * 2
+                score[-1] = score[-1] * 2
+            else: # 1개 일때
+                score[-1] = score[-1] * 2
+        elif i == '#':
+            score[-1] = score[-1] * -1
+    
+    return sum(score)
+
+
+## 다른 사람의 풀이
+import re
+
+
+def solution(dartResult):
+    bonus = {'S' : 1, 'D' : 2, 'T' : 3}
+    option = {'' : 1, '*' : 2, '#' : -1}
+    p = re.compile('(\d+)([SDT])([*#]?)')
+    dart = p.findall(dartResult)
+    for i in range(len(dart)):
+        if dart[i][2] == '*' and i > 0:
+            dart[i-1] *= 2
+        dart[i] = int(dart[i][0]) ** bonus[dart[i][1]] * option[dart[i][2]]
+
+    answer = sum(dart)
+    return answer
