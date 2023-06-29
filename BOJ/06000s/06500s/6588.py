62d7ff71c8efc4738d9940f9be934c328cf42560
gracekim527 <lemonherb0323@naver.com>
2023-06-29 14:18:20 +0900

    :white_check_mark: [BOJ] Add 6588

--- BOJ/06000s/06500s/6588.py
+++ BOJ/06000s/06500s/6588.py
@@ -1,21 +1,20 @@
-r = 1000000
-check = [True for _ in range(r)]
-for i in range(2, int(r**0.6)):
-    if check[i]==True:
-        for j in range(i*2, r, i): 
-            if check[j] == True:
-                check[j] = False
-
 import sys
 input = sys.stdin.readline
 
+r = 1000000
+check = [True for _ in range(r)]
+for i in range(2, int(r ** 0.5)):
+    if check[i] == True:
+        for j in range(i * 2, r, i): 
+            if check[j] == True:
+                check[j] = False
+
 while(True):
     n = int(input())
-
     if n == 0: 
         break
-    for i in range(3, r):
+    for i in range(3, r, 2): #홀수부터 시작, 홀수만 체크안하고 짝수까지 체크하게 되면 시간 초과가 발생
         if check[i] == True:
-            if check[n-i] == True :
-                print("%d = %d + %d"%(n , i , n-i))
+            if check[n - i] == True:
+                print("%d = %d + %d" % (n , i , n - i))
                 break
